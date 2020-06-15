cca68ce2bcab515afd77d0d4604a0496301b438c
Ardi <oliviergiroud1998@gmail.com>
2020-06-15 14:51:40 +0700

    Add Index Page and Individual Post Page

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,4 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/dashboard', 'HomeController@dashboard')->name('dashboard');
 Route::resource('profile', 'ProfileController');
+Route::resource('post', 'PostController');
